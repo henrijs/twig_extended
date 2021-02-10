d760dda60f100464f48bbe6dfd3c63033f2f3b3d
GitHub Action <action@github.com>
2021-02-10 10:42:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/henrijs/twigextended/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/henrijs/twigextended/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.henrijs.twigextended.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.henrijs.twigextended.MyBundle
 
 class MyApplicationService {
 
